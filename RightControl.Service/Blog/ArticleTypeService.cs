0e24ff75a78cbbe7fbb0a63e895fa0724f0fee62
Liu_Cabbage <178899573@qq.com>
2019-06-29 18:02:55 +0800

    Blog

--- RightControl.Service/Blog/ArticleTypeService.cs
+++ RightControl.Service/Blog/ArticleTypeService.cs
@@ -12,7 +12,7 @@ namespace RightControl.Service
     {
         public dynamic GetListByFilter(ArticleTypeModel filter, PageInfo pageInfo)
         {
-            throw new NotImplementedException();
+            return GetListByFilter(filter, pageInfo, null);
         }
     }
 }
---
+++ RightControl.WebApp/Areas/Blog/Controllers/ArticleTypeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RightControl.WebApp.Areas.Admin.Controllers;
+using RightControl.Model;
+using RightControl.IService;
+
+namespace RightControl.WebApp.Areas.Blog.Controllers
+{
+    public class ArticleTypeController : BaseController
+    {
+        public IArticleTypeService articleTypeService { get; set; }
+        // GET: Blog/ArticleType
+        public override ActionResult Index(int? Id)
+        {
+            base.Index(Id);
+            return View();
+        }
+        [HttpGet]
+        public JsonResult List(ArticleTypeModel filter, PageInfo pageInfo)
+        {
+            var result = articleTypeService.GetListByFilter(filter, pageInfo);
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(ArticleTypeModel model)
+        {
+            model.CreateOn = DateTime.Now;
+            var result = articleTypeService.CreateModel(model) ? SuccessTip() : ErrorTip();
+            return Json(result);
+        }
+        public ActionResult Detail(int Id)
+        {
+            var model = articleTypeService.ReadModel(Id);
+            return View(model);
+        }
+        public ActionResult Edit(int Id)
+        {
+            var model = articleTypeService.ReadModel(Id);
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Edit(ArticleTypeModel model)
+        {
+            var result = articleTypeService.UpdateModel(model) ? SuccessTip() : ErrorTip();
+            return Json(result);
+        }
+        [HttpPost]
+        public ActionResult Delete(int Id)
+        {
+            //这里删除文章类型，没有删除类型下的文章，后面改
+            var result = articleTypeService.DeleteModel(Id) ? SuccessTip() : ErrorTip();
+            return Json(result);
+        }
+    }
+}
